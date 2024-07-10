eff9634116e02417c51f7d15bb011b44c2b403b8
sleepblackrabbit <sleepblackrabbit@gmail.com>
2024-07-10 21:03:53 +0900

    chap second test third code

--- chap02test01.R
+++ chap02test01.R
@@ -3,3 +3,14 @@ ex_vector5
 
 cate_vector5 <-factor(ex_vector5, labels = c("Apple", "Banana", "Cheery"))
 cate_vector5
+
+x <- c(1,2,3,4,5,6)
+matrix(x, nrow = 2, ncol= 3)
+matrix(x, nrow = 3, ncol = 2)
+
+y <- c(1,2,3,4,5,6)
+array(y, dim = c(2, 2, 3))
+
+list1 = list(c(1,2,3,4), "hello")
+list1
+
